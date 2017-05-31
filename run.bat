7a6d859e91d6b1a1d772fabe13b1d325a91b1306
Robert <bielas.robert95@gmail.com>
2017-05-31 20:59:50 +0200

    printing in bat changed

--- run.bat
+++ run.bat
@@ -1,12 +1,19 @@
 @ECHO OFF
+
 stack build --haddock
+
 REM .stack-work\install\ff4c9dd0\bin\Parser-exe.exe
+
 stack path --local-install-root > f
 set /p var=<f
-set var=%var%\bin\Parser-exe.exe
-ECHO. & ECHO EXECUTING MAIN & ECHO.
-%var%
 del /f f
+set var=%var%\bin\Parser-exe.exe
+
+ECHO.
+ECHO EXECUTING MAIN  
+ECHO ==================================================================================================== & ECHO. 
+%var%
+ECHO. & ECHO ==================================================================================================== & ECHO. 
 
 stack clean
 stack test --coverage
